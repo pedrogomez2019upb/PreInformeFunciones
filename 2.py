72924952bc7b1e33e7ec834b94f91d1e5b8ea018
Pedro Gómez <pipe22007@gmail.com>
2019-03-27 18:01:20 -0500

    Se añade el avanze del trabajo.

--- 2.py
+++ 2.py
@@ -8,3 +8,13 @@ x1=a**b
 print("La respuesta de {} elevado a {} es: {}".format(a,b,x1))
 #Se hace otra vez la operación mediante haciendolo a la inversa
 x2=b**a
+#Se imprime el segundo resultado
+print("La inversa de la respuesta es: {}".format(x2))
+#Se pone las condicionales para saber cual es mayor que otro
+if x1>x2:
+    print("La primera respuesta es mayor que la segunda. Gracias por utilizar el programa.")
+else:
+    if x1=x2:
+        print("Las dos respuestas son iguales. Gracias por utilizar el programa")
+    else:
+        print("La segunda respuesta es mayor a la primera. Gracias por utilizar el programa")
