42e236bddeef0fbbc461348499406b8920e33814
Pedro Gómez <pipe22007@gmail.com>
2019-03-27 18:30:45 -0500

    Se añade el archivo 3.py funcionando y compilando.

--- 3.py
+++ 3.py
@@ -1,18 +1,13 @@
 #Realizar una función que permita saber si un número es o no primo
 #Primero hay que recibir el número a analizar
 numero=int(input("Hola! Vamos a ver si el número ingresado es primo. Por favor ingrese el número a analizar: "))
-#Se crea una función para números primos
-def primo(numero):
-#Se hace un bucle para declarar cuando es primo
-    for i in range(2,numero):
-        if (numero%i)==0:
-            # es divisible
-            return False
-    return True
-#Ponemos ahora las condicionales necesarias para saber si es primo
-if numero==0:
-    break
-if primo(numero):
-    print ("\nEl numero %s es primo" % numero)
+#Se crea un for para números primos y su contador que empice desde cero
+contador = 0
+for i in range(1,numero+1):
+    if (numero% i)==0:
+        contador = contador + 1
+if contador==2:
+    print ("El número ingresado es primo. Gracias por utilizar el programa.)
 else:
-    print ("\nEl numero %s NO es primo" % numero)
+    print ("El número ingesado no es primo. Gracias por utilizar el programa.")
+#Desarrollado por Pedro Gómez / ID:000396221
